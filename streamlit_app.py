f86668a4d0166c8dee020825697cb43d2aa4fc68
nicolalopomo <nicola.lopomo@polimi.it>
2025-04-08 21:36:05 +0000

    Furhter changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import os
 
 # Streamlit Google Sheets connection
 from streamlit_gsheets import GSheetsConnection
@@ -6,19 +8,38 @@ from streamlit_gsheets import GSheetsConnection
 # Create a connection object
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Get the dataframe via read method
-df = conn.read()
-
-# Display the dataframe
-st.dataframe(df)
-
+conn.read()
 # Customize the reading
-df = conn.read(
-    worksheet="Sheet1",
-    ttl="10m",
-    usecols=[0, 1],
-    nrows=3,
-)
+df = conn.read()
 
 # Display the new dataframe
 st.dataframe(df)
+
+# Define file cwd path
+myCWD = os.getcwd()
+# Define the path where to save the file
+csv_file_path = os.path.join(myCWD, "my_data.csv")
+
+# Write CSV
+df.to_csv(csv_file_path, index=False) # Not writing row names
+
+st.success(f"Data saved to {csv_file_path}")
+
+
+# Display an image
+st.image("https://static.streamlit.io/examples/cat.jpg",
+         caption="This is a cat!",
+         width=100)
+
+# Display a video
+# Rember to upload the video in the project folder
+video = open("my_video.mp4", "rb")
+video_bytes = video.read()
+st.video(video_bytes)
+
+
+st.logo(
+    "https://images.seeklogo.com/logo-png/44/1/streamlit-logo-png_seeklogo-441815.png",
+    link="https://streamlit.io/gallery",
+    icon_image="https://images.seeklogo.com/logo-png/44/1/streamlit-logo-png_seeklogo-441815.png",
+)
