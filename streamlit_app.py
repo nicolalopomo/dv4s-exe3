2b75aca56b8707e9b29a95722afe0be258467f57
nicolalopomo <nicola.lopomo@polimi.it>
2025-04-08 20:10:50 +0000

    Some changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# Streamlit Google Sheets connection
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+# Get the dataframe via read method
+df = conn.read()
+
+# Display the dataframe
+st.dataframe(df)
+
+# Customize the reading
+df = conn.read(
+    worksheet="Sheet1",
+    ttl="10m",
+    usecols=[0, 1],
+    nrows=3,
 )
+
+# Display the new dataframe
+st.dataframe(df)
